30671c68f632d9ca9ae7ba1d36fe6d7d90e2ec85
semklauke <privat@semklauke.de>
2020-03-28 21:03:29 +0100

    added frequencies

---
+++ src/codelists/frequencies.ts
@@ -0,0 +1,9 @@
+export enum Frequency {
+    Annual         = "A",
+    Daily          = "D",
+    HalfYearly     = "H",
+    Semester       = "S",
+    Monthly        = "M",
+    Quarterly      = "Q",
+    Weekly         = "W"
+}
